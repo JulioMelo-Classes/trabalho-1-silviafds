29c0a1a4a10e37ffc26da6eaca15147bafc7eadf
Brenda Dantas <brendasod98@gmail.com>
2021-06-30 15:07:34 -0300

    Adicionando..

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[]){
-    int v[5];
-    int i = 0, x, num_jogadas;
-    float valor;
+    int v[5], i = 0, x, num_jogadas, j = 0, tam = 0;
+    float v_float[5], valor_da_aposta, y;
     stringstream ss;
+    stringstream xx;
 
     ifstream arquivo_tst_keno;
     string linha;
@@ -20,27 +21,42 @@ int main(int argc, char *argv[]){
         while(getline(arquivo_tst_keno, linha)){
             cout << linha << endl;
             ss << linha << endl;
+            xx << linha << endl;
         }
     } else{
         cout << "NÃO FOI POSSIVEL ABRIR O ARQUIVO\n";
     }
-    while(ss >> x){//aplicando o stringstream
-        //cout << x*1 << endl;
-        v[i] = x*1;//guardando na variavel
-        i++;
+   
+    while(xx >> y){//aplicando o stringstream
+        //cout << y*1 << endl;
+        v_float[j] = y;//guardando na variavel
+        j++;
+        tam++;
     }
+
     cout << "\n==CONVERTIDOS==\n";
     for(int i=0; i<5;i++){
-        cout << v[i] << endl;
+        cout << v_float[i] << endl;
     }
-    valor = v[0];
-    float valor_aposta = (float)valor;//valor da aposta
-    num_jogadas = v[1];
+    
+    valor_da_aposta = v_float[0];
+    cout << "\nValor da aposta: " << valor_da_aposta << fixed << setprecision(1) << "\n";
 
-    cout << "\nValor convertido: " << valor_aposta << fixed << setprecision(1) << "\n";
+    num_jogadas = v_float[1];
     cout << "\nNúmero de jogadas: " << num_jogadas << "\n";
+    cout << "\nTamanho do vetor: " << tam << "\n";
 
-    float n = valor_aposta;
-    cout << "\n" << n << endl;
+    tam = tam - 2;
+    int num_escolhidos[tam], jj = 2;
+
+    vector<int> v1;
+    for(int l=0; l<tam; l++){
+        v1.push_back(v_float[jj]);
+        jj++;
+    }
+    for(i = 0; i<tam; i++){
+        cout << v1[i] << endl;
+    }
+
     return 0;
 }
